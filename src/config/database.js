fecec0949c8f642210444e939d05de873a3cdbd5
Stwn25 <msetiawanwibisono@gmail.com>
2025-10-31 19:06:10 +0700

    fix

--- src/config/database.js
+++ src/config/database.js
@@ -31,19 +31,20 @@ module.exports = {
     dialect: 'mysql',
     logging: false,
   },
-  // production: {
-  //   username: process.env.DB_USERNAME,
-  //   password: process.env.DB_PASSWORD,
-  //   database: process.env.DB_NAME,
-  //   host: process.env.DB_HOST,
-  //   dialect: 'mysql',
-  //   logging: false,
-  //   timezone: '+07:00',
-  //   pool: { 
-  //     max: 10,
-  //     min: 0,
-  //     acquire: 30000,
-  //     idle: 10000
-  //   }
-  // }
+  production: {
+    // username: process.env.DB_USERNAME,
+    // password: process.env.DB_PASSWORD,
+    // database: process.env.DB_NAME,
+    // host: process.env.DB_HOST,
+    use_env_variable: 'DB_URL',
+    dialect: 'mysql',
+    logging: false,
+    timezone: '+07:00',
+    pool: { 
+      max: 10,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    }
+  }
 };
