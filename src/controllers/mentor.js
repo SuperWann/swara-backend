6e85faddb3d473e7e4960523df06133f6bed55e1
Stwn25 <msetiawanwibisono@gmail.com>
2025-11-06 16:48:29 +0700

    fix: mentor

--- src/controllers/mentor.js
+++ src/controllers/mentor.js
@@ -1,5 +1,49 @@
-const { User, Role, Gender, SkorSwara, PodiumText, PodiumCategory } = require('../models');
+const { MentorActivity, User } = require('../models');
 
 class MentorController {
-    
+    static async getAllActivities(req, res) {
+        try {
+            const activitiesByUserId = await MentorActivity.findAll({
+                where: { user_id: req.params.id },
+                include: [{
+                    model: User,
+                    as: 'mentor',
+                    attributes: ['user_id', 'full_name']
+                }]
+            });
+            res.json({
+                success: true,
+                message: 'Activities retrieved successfully',
+                count: activitiesByUserId.length,
+                data: activitiesByUserId
+            });
+        } catch (error) {
+            res.status(500).json({
+                success: false,
+                message: 'Failed to get activities',
+                error: error.message
+            });
+        }
+    }
+
+    static async createActivity(req, res) {
+        try {
+            const { user_id, judul_aktivitas, deskripsi } = req.body;
+            const newActivity = await MentorActivity.create({ user_id, judul_aktivitas, deskripsi });
+            res.json({
+                success: true,
+                message: 'Activity created successfully',
+                data: newActivity
+            });
+        } catch (error) {
+            res.status(500).json({
+                success: false,
+                message: 'Failed to create activity',
+                error: error.message
+            });
+        }
+    }
+
 }
+
+module.exports = MentorController;
--- src/routes/mentor.js
+++ src/routes/mentor.js
@@ -3,3 +3,8 @@ const MentorController = require('../controllers/mentor');
 const { checkRole, authenticateToken } = require('../middleware/auth');
 
 const router = express.Router();
+
+router.get('/activities/:id', authenticateToken, MentorController.getAllActivities);
+router.post('/activity', authenticateToken, MentorController.createActivity);
+
+module.exports = router;
